aa8bfa66ef0e01578edfce2e423a52a10e1ddf3a
Santiago Serna <sanserve91@gmail.com>
2019-01-27 20:10:01 -0500

    Se aggregan estilos de timeline

--- src/components/education/education.js
+++ src/components/education/education.js
@@ -1,30 +1,36 @@
 import React from 'react';
+import { Subscribe } from 'unstated';
+
+import { PersonDataContainer } from '../../containers/person-data.container';
 
 import educationStyles from './education.module.scss';
+import globalComponentsStyles from '../../styles/modules/global-components.module.scss';
 import typographyStyles from '../../styles/modules/typography.module.scss';
 import SectionContainer from '../section-container/section-container';
 import Card from '../card/card';
 
 const Education = () => (
-  <SectionContainer>
-    <Card>
-      <div className={educationStyles.contentLeft}>
-        <h3 className={typographyStyles.sectionTitle}>Educación</h3>
-        <span>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, quo.
-          Officiis, deleniti nihil aspernatur quod ea incidunt quam architecto
-          vel.
-        </span>
-      </div>
-      <div className={educationStyles.contentRight}>
-        <span>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, quo.
-          Officiis, deleniti nihil aspernatur quod ea incidunt quam architecto
-          vel.
-        </span>
-      </div>
-    </Card>
-  </SectionContainer>
+  <Subscribe to={[PersonDataContainer]}>
+    {({ state: person }) => (
+      <SectionContainer>
+        <Card>
+          <div className={educationStyles.contentLeft}>
+            <h3 className={typographyStyles.sectionTitle}>Educación</h3>
+            <span>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis,
+              quo. Officiis, deleniti nihil aspernatur quod ea incidunt quam
+              architecto vel.
+            </span>
+          </div>
+          <div className={educationStyles.contentRight}>
+            <ul className={globalComponentsStyles.timeline}>
+              <li>hola</li>
+            </ul>
+          </div>
+        </Card>
+      </SectionContainer>
+    )}
+  </Subscribe>
 );
 
 Education.propTypes = {};
