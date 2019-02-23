42f1e077b68a8cc9da00d5b5198258fa5bd1b0bb
Santiago Serna <sanserve91@gmail.com>
2019-02-23 14:11:52 -0500

    Fix bg-image-seccion

---
+++ src/lib/bg-img-section/bg-img-section.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Img from 'gatsby-image';
+
+const BgImgSection = ({ text, background }) => (
+  <div className="bg-img-section">
+    <Img fluid={background} className="bg-image-container" />
+    <div className="text-wrapper">
+      <h2 className="text-content">{text}</h2>
+    </div>
+
+    <style jsx>{`
+      .bg-img-section :global(.bg-image-container) {
+        left: 0;
+        top: calc(50% - ${background.presentationHeight / 2}px);
+      }
+    `}</style>
+
+    <style jsx>{`
+      .bg-img-section {
+        position: relative;
+        max-height: 350px;
+        overflow: hidden;
+
+        @above mobile {
+          max-height: 450px;
+        }
+
+        @above tablet {
+          max-height: 500px;
+        }
+      }
+
+      .text-wrapper {
+        position: absolute;
+        top: 0;
+        left: 0;
+        padding: 10px;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+
+        @from-width tablet {
+          padding: 0 100px;
+        }
+
+        @from-width desktop {
+          padding: 0 150px;
+        }
+      }
+
+      .text-content {
+        color: white;
+        margin: 0 auto;
+        text-align: center;
+        text-shadow: 1px 1px 10px black;
+        max-width: 1200px;
+      }
+    `}</style>
+  </div>
+);
+
+BgImgSection.propTypes = {
+  text: PropTypes.string.isRequired,
+  background: PropTypes.shape({
+    base64: PropTypes.string,
+    aspectRatio: PropTypes.number,
+    src: PropTypes.string,
+    srcSet: PropTypes.string,
+    sizes: PropTypes.string,
+    presentationHeight: PropTypes.number
+  }).isRequired
+};
+
+export default BgImgSection;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,6 +5,7 @@ import { graphql } from 'gatsby';
 import { ThemeContext } from '../layouts';
 import Seo from '../lib/seo';
 import SectionContainer from '../lib/section-container';
+import BgImgSection from '../lib/bg-img-section';
 import HeroHeader from '../components/hero-header';
 import Resume from '../components/resume';
 import Skills from '../components/skills';
@@ -12,65 +13,13 @@ import Education from '../components/education';
 import Experience from '../components/experience';
 import Contact from '../components/contact';
 
-// FIXME: hacer componente generico en lib
-const BgImgSection = ({
-  text,
-  backgrounds: { bgCondingDesktop, bgCondingTablet, bgCondingMobile }
-}) => (
-  <>
-    <div>
-      <h1>{text}</h1>
-    </div>
-
-    <style jsx>{`
-      div {
-        background-size: cover;
-        background-attachment: fixed;
-        background-position: 50% 50%;
-        background-image: url(${bgCondingMobile});
-        padding: 30px;
-
-        @from-width tablet {
-          background-image: url(${bgCondingTablet});
-          padding: 200px 100px;
-        }
-
-        @from-width desktop {
-          background-image: url(${bgCondingDesktop});
-        }
-      }
-
-      h1 {
-        color: white;
-        margin: 0 auto;
-        text-align: center;
-        text-shadow: 1px 1px 10px black;
-        max-width: 1200px;
-      }
-    `}</style>
-  </>
-);
-
 const IndexPage = ({ data }) => {
   const theme = useContext(ThemeContext);
   const {
-    bgCondingDesktop: {
-      childImageSharp: {
-        fluid: { src: bgCondingDesktop }
-      }
-    },
-    bgCondingTablet: {
-      childImageSharp: {
-        fluid: { src: bgCondingTablet }
-      }
-    },
-    bgCondingMobile: {
-      childImageSharp: {
-        fluid: { src: bgCondingMobile }
-      }
+    bgConding: {
+      childImageSharp: { fluid: bgConding }
     }
   } = data;
-  const backgrounds = { bgCondingDesktop, bgCondingTablet, bgCondingMobile };
 
   return (
     <>
@@ -96,7 +45,7 @@ const IndexPage = ({ data }) => {
 
       <SectionContainer style={{ paddingLeft: 0, paddingRight: 0 }}>
         <BgImgSection
-          backgrounds={backgrounds}
+          background={bgConding}
           text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis consectetur voluptates et incidunt dolore veniam suscipit voluptatibus blanditiis fugiat corporis."
         />
       </SectionContainer>
@@ -116,24 +65,11 @@ export default IndexPage;
 
 export const query = graphql`
   query {
-    bgCondingDesktop: file(relativePath: { eq: "bg-2.jpg" }) {
+    bgConding: file(relativePath: { eq: "bg-2.jpg" }) {
       childImageSharp {
         fluid(maxWidth: 1200, quality: 100) {
-          src
-        }
-      }
-    }
-    bgCondingTablet: file(relativePath: { eq: "bg-2.jpg" }) {
-      childImageSharp {
-        fluid(maxWidth: 800, maxHeight: 1100, quality: 90) {
-          src
-        }
-      }
-    }
-    bgCondingMobile: file(relativePath: { eq: "bg-2.jpg" }) {
-      childImageSharp {
-        fluid(maxWidth: 450, maxHeight: 850, quality: 90) {
-          src
+          ...GatsbyImageSharpFluid
+          presentationHeight
         }
       }
     }
