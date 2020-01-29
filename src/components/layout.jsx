eb3a0353e17f6dd7bd1421617d9d891b18e21b94
Santiago Serna <sanserve91@gmail.com>
2020-01-28 21:32:25 -0500

    updated main layout

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Global, css } from '@emotion/core';
 import { useTheme } from 'emotion-theming';
 
 import MainFooter from 'app-components/main-footer';
@@ -10,70 +9,15 @@ const Layout = ({ children, location }) => {
   const theme = useTheme();
 
   return (
-    <>
-      <Global
-        styles={css`
-          * {
-            box-sizing: border-box;
-          }
-
-          code[class*='language-'] {
-            font-size: 1rem;
-          }
-
-          .gatsby-resp-image-wrapper {
-            max-width: none !important;
-          }
-
-          .gatsby-highlight-code-line {
-            background-color: #7a7a7a;
-            display: block;
-            margin-right: -1em;
-            margin-left: -1em;
-            padding-right: 1em;
-            padding-left: 0.75em;
-            border-left: 0.25em solid #f99;
-          }
-
-          .gatsby-highlight {
-            background-color: #2d2d2d;
-            border-radius: 0.3em;
-            margin: 0.5em 0;
-            padding: 0.2em;
-            overflow: auto;
-
-            pre[class*='language-'] {
-              background-color: transparent;
-              margin: 0;
-              padding: 0;
-              overflow: initial;
-              float: left;
-              min-width: 100%;
-            }
-
-            pre[class*='language-'].line-numbers {
-              padding: 0;
-              padding-left: 3.2em;
-              overflow: initial;
-
-              .line-numbers-rows {
-                padding-left: 0.5em;
-              }
-            }
-          }
-        `}
-      />
-      <div
-        css={{
-          minHeight: '100vh',
-          background: theme.colors.gray[100]
-        }}
-      >
-        <MainNav showHeroImg={location.pathname !== '/'} />
-        <main>{children}</main>
-        <MainFooter />
-      </div>
-    </>
+    <div
+      css={{
+        background: theme.colors.gray[100]
+      }}
+    >
+      <MainNav showHeroImg={location.pathname !== '/'} />
+      <main>{children}</main>
+      <MainFooter />
+    </div>
   );
 };
 
